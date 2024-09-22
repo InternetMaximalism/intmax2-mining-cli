1971fd08eac6fae24419e2e091fdb25ede504d3b
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-09-22 16:29:48 +0800

    print address

--- v1-cli/src/cli/private_data.rs
+++ v1-cli/src/cli/private_data.rs
@@ -1,8 +1,10 @@
 use crate::{
+    external_api::contracts::utils::get_wallet,
     state::private_data::{load_encrypted_private_data, write_encrypted_private_data, PrivateData},
     utils::network::get_network,
 };
 use dialoguer::{Input, Password};
+use ethers::signers::Signer;
 use ethers::types::{H160, H256};
 
 /// Require input or load private data
@@ -14,18 +16,27 @@ pub async fn set_private_data() -> anyhow::Result<PrivateData> {
             Ok(private_data)
         }
         None => {
-            let deposit_private_key: String = Password::new()
+            let deposit_private_key_str: String = Password::new()
                 .with_prompt(format!(
                     "Private key of deposit account on {}",
                     get_network(),
                 ))
                 .validate_with(|input: &String| validate_private_key(input))
                 .interact()?;
-            let claim_private_key: String = Password::new()
+
+            let deposit_private_key = deposit_private_key_str.parse()?;
+            let deposit_address = get_wallet(deposit_private_key).await?.address();
+            println!("Deposit address: {:?}", deposit_address);
+
+            let claim_private_key_str: String = Password::new()
                 .with_prompt(format!("Private key of claim account on {}", get_network(),))
                 .validate_with(|input: &String| validate_private_key(input))
                 .interact()?;
-            let withdrawal_address: String = Input::new()
+            let claim_private_key = claim_private_key_str.parse()?;
+            let claim_address = get_wallet(claim_private_key).await?.address();
+            println!("Claim address: {:?}", claim_address);
+
+            let withdrawal_address_str: String = Input::new()
                 .with_prompt(format!(
                     "Address of withdrawal account on {}",
                     get_network(),
@@ -33,9 +44,9 @@ pub async fn set_private_data() -> anyhow::Result<PrivateData> {
                 .validate_with(|input: &String| validate_address(input))
                 .interact()?;
             let private_data = PrivateData::new(
-                &deposit_private_key,
-                &claim_private_key,
-                &withdrawal_address,
+                &deposit_private_key_str,
+                &claim_private_key_str,
+                &withdrawal_address_str,
             )
             .await?;
 
