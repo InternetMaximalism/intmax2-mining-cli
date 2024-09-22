219e9bd3d4a84f0a4f6ee36f47a49a48e16d7c6c
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-09-22 14:26:18 +0800

    fix sync

--- v1-cli/src/services/sync.rs
+++ v1-cli/src/services/sync.rs
@@ -39,6 +39,8 @@ pub async fn sync_trees(
                     .await?;
         }
         None => {
+            *last_deposit_block_number =
+                sync_to_latest_deposit_tree(deposit_hash_tree, *last_deposit_block_number).await?;
             *last_update = now; // update last_update to now
         }
     }
@@ -50,13 +52,25 @@ async fn sync_to_latest_deposit_tree(
     from_block: u64,
 ) -> anyhow::Result<u64> {
     let events = get_deposit_leaf_inserted_event(from_block).await?;
+    let next_deposit_index = deposit_hash_tree.tree.len();
+    let mut to_append = events
+        .iter()
+        .filter(|event| event.deposit_index as usize >= next_deposit_index)
+        .collect::<Vec<_>>();
+    to_append.sort_by_key(|event| event.deposit_index);
+
     let mut to_block_number = from_block;
-    for event in events {
-        if deposit_hash_tree.tree.len() == event.deposit_index as usize {
-            deposit_hash_tree.push(event.deposit_hash);
-            to_block_number = event.block_number;
-        }
+    for event in to_append {
+        ensure!(
+            event.deposit_index as usize == deposit_hash_tree.tree.len(),
+            "Deposit index mismatch: expected {}, got {}",
+            deposit_hash_tree.tree.len(),
+            event.deposit_index
+        );
+        deposit_hash_tree.push(event.deposit_hash);
+        to_block_number = event.block_number;
     }
+
     let expected_root = get_deposit_root().await?;
     let actual_root = deposit_hash_tree.get_root();
     ensure!(
@@ -66,4 +80,29 @@ async fn sync_to_latest_deposit_tree(
         actual_root
     );
     Ok(to_block_number)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::utils::{
+        deposit_hash_tree::DepositHashTree, eligible_tree_with_map::EligibleTreeWithMap,
+    };
+
+    #[tokio::test]
+    async fn sync_to_latest_deposit_tree() {
+        let mut deposit_hash_tree = DepositHashTree::new();
+        let mut eligible_tree = EligibleTreeWithMap::new();
+        let mut last_deposit_block_number = 0;
+        let mut last_update = chrono::NaiveDateTime::default();
+        super::sync_trees(
+            &mut last_deposit_block_number,
+            &mut last_update,
+            &mut deposit_hash_tree,
+            &mut eligible_tree,
+        )
+        .await
+        .unwrap();
+
+        dbg!(deposit_hash_tree.tree.len());
+    }
 }
