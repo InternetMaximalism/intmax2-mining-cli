e4985e433b1eba3be7fa0fb49c4451ac1100cfc1
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-09-26 21:00:36 +0800

    load env

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,17 +1,17 @@
 use availability::check_avaliability;
 use console::print_status;
-use load_env::Config;
+use dialoguer::Password;
 
 use crate::{
     services::{claim_loop, exit_loop, mining_loop},
-    state::{mode::RunMode, prover::Prover, state::State},
-    utils::network::get_network,
+    state::{keys::Keys, mode::RunMode, prover::Prover, state::State},
+    utils::{encryption::decrypt, env_config::EnvConfig, network::get_network},
 };
 
 pub mod availability;
 pub mod balance_validation;
 pub mod console;
-pub mod load_env;
+// pub mod load_env;
 
 pub async fn run(mode: RunMode) -> anyhow::Result<()> {
     println!(
@@ -21,12 +21,21 @@ pub async fn run(mode: RunMode) -> anyhow::Result<()> {
     );
     check_avaliability().await?;
 
-    let config = Config::load().await?;
+    let config = EnvConfig::import_from_env()?;
+    let keys = if let Some(keys) = &config.keys {
+        keys.clone()
+    } else {
+        // require password to decrypt keys
+        let password = Password::new().with_prompt("Password").interact()?;
+        let keys: Keys = decrypt(&password, config.encrypted_keys.as_ref().unwrap())?;
+        keys
+    };
+
     let mut state = State::new();
     let prover_future = tokio::spawn(async { Prover::new() });
 
     // valance validation
-    balance_validation::balance_validation(&mut state, mode, config.clone()).await?;
+    balance_validation::balance_validation(&mut state, mode, &config, &keys).await?;
 
     // wait for prover to be ready
     println!(); // newline because print_status clears the last line
@@ -37,16 +46,10 @@ pub async fn run(mode: RunMode) -> anyhow::Result<()> {
     // main loop
     match mode {
         RunMode::Mining => {
-            mining_loop(
-                &mut state,
-                &config.keys,
-                config.mining_unit,
-                config.mining_times,
-            )
-            .await?
+            mining_loop(&mut state, &keys, config.mining_unit, config.mining_times).await?
         }
-        RunMode::Claim => claim_loop(&mut state, &config.keys).await?,
-        RunMode::Exit => exit_loop(&mut state, &config.keys).await?,
+        RunMode::Claim => claim_loop(&mut state, &keys).await?,
+        RunMode::Exit => exit_loop(&mut state, &keys).await?,
         RunMode::Config => unreachable!(),
     }
 
--- src/services/gas_validation.rs
+++ src/services/gas_validation.rs
@@ -1,11 +1,11 @@
 use crate::{
-    cli::{console::print_warning, load_env::load_max_gas_price},
+    cli::console::print_warning,
     external_api::contracts::utils::get_gas_price,
-    utils::config::Settings,
+    utils::{config::Settings, env_config::EnvConfig},
 };
 
 pub async fn await_until_low_gas_price() -> anyhow::Result<()> {
-    let max_gas_price = load_max_gas_price()?;
+    let max_gas_price = EnvConfig::import_from_env()?.max_gas_price;
     let settings = Settings::load()?;
     let high_gas_retry_inverval_in_sec = settings.service.high_gas_retry_inverval_in_sec;
     let _url = settings.service.repository_url;
@@ -13,15 +13,16 @@ pub async fn await_until_low_gas_price() -> anyhow::Result<()> {
         let current_gas_price = get_gas_price().await?;
         if current_gas_price <= max_gas_price {
             log::info!(
-                "Current gas price: {:?} is lower than max gas price: {:?}",
-                current_gas_price,
-                max_gas_price
+                "Current gas price: {} GWei is lower than max gas price: {} GWei",
+                ethers::utils::format_units(current_gas_price.clone(), "gwei").unwrap(),
+                ethers::utils::format_units(max_gas_price.clone(), "gwei").unwrap(),
             );
             break;
         }
         print_warning(format!(
-            "Current gas price: {:?} is higher than max gas price: {:?}. Please see README",
-            current_gas_price, max_gas_price,
+            "Current gas price: {} GWei is higher than max gas price: {}  GWei. Please see README",
+            ethers::utils::format_units(current_gas_price.clone(), "gwei").unwrap(),
+            ethers::utils::format_units(max_gas_price.clone(), "gwei").unwrap(),
         ));
         tokio::time::sleep(std::time::Duration::from_secs(
             high_gas_retry_inverval_in_sec,
