b585322835a8aee4f27c8ea01bb82e5455f748f3
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-09-28 15:48:31 +0800

    wip

--- src/services/claim/mod.rs
+++ src/services/claim/mod.rs
@@ -19,12 +19,10 @@ pub async fn claim_task(
     key: &Key,
     assets_status: &AssetsStatus,
 ) -> anyhow::Result<()> {
-    if !assets_status.not_claimed_indices.is_empty() {
-        for events in assets_status.get_not_claimed_events().chunks(MAX_CLAIMS) {
-            single_claim_task(state, key, &events)
-                .await
-                .map_err(|e| CLIError::InternalError(format!("Failed to claim: {:#}", e)))?;
-        }
+    for events in assets_status.get_not_claimed_events().chunks(MAX_CLAIMS) {
+        single_claim_task(state, key, &events)
+            .await
+            .map_err(|e| CLIError::InternalError(format!("Failed to claim: {:#}", e)))?;
     }
     Ok(())
 }
--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,17 +1,16 @@
-use claim::claim_task;
-use ethers::types::{H256, U256};
-use intmax2_zkp::circuits::withdrawal;
-use mining::mining_task;
-use rand::Rng as _;
-
 use crate::{
     cli::{
         balance_validation::validate_deposit_address_balance,
         console::{print_assets_status, print_status},
     },
+    external_api::contracts::utils::{get_account_nonce, get_balance},
     state::{key::Key, state::State},
-    utils::{config::Settings, errors::CLIError},
+    utils::config::Settings,
 };
+use claim::claim_task;
+use ethers::types::{Address, H256, U256};
+use mining::mining_task;
+use rand::Rng as _;
 
 pub mod assets_status;
 pub mod claim;
@@ -70,17 +69,15 @@ pub async fn mining_loop(
     }
 }
 
-pub async fn exit_loop(state: &mut State, mining_keys: &Keys) -> anyhow::Result<()> {
-    for key in mining_keys.to_keys().iter() {
-        print_status(format!("Exit loop for {:?}", key.deposit_address));
+pub async fn exit_loop(state: &mut State, withdrawal_private_key: H256) -> anyhow::Result<()> {
+    let mut key_number = 0;
+    loop {
+        let key = Key::new(withdrawal_private_key, key_number);
+        if !is_address_used(key.deposit_address).await {
+            return Ok(());
+        }
         loop {
-            let assets_status = state.sync_and_fetch_assets(key).await.map_err(|e| {
-                CLIError::NetworkError(format!(
-                    "Failed while fetching assets status for {:?}: {:?}",
-                    key.deposit_address, e
-                ))
-            })?;
-
+            let assets_status = state.sync_and_fetch_assets(&key).await?;
             if assets_status.pending_indices.is_empty()
                 && assets_status.rejected_indices.is_empty()
                 && assets_status.not_withdrawn_indices.is_empty()
@@ -89,44 +86,34 @@ pub async fn exit_loop(state: &mut State, mining_keys: &Keys) -> anyhow::Result<
                     "All deposits are withdrawn for {:?}. Exiting.",
                     key.deposit_address,
                 ));
+                key_number += 1;
                 break;
             }
-
-            mining_task(state, key, &assets_status, false, true, 0.into()).await?;
+            mining_task(state, &key, &assets_status, false, true, 0.into()).await?;
 
             common_loop_cool_down().await;
         }
     }
-
-    Ok(())
 }
 
-pub async fn claim_loop(state: &mut State, keys: &Keys) -> anyhow::Result<()> {
-    for key in keys.to_keys().iter() {
-        print_status(format!("Claim loop for {:?}", key.deposit_address));
-        loop {
-            let assets_status = state.sync_and_fetch_assets(key).await.map_err(|e| {
-                CLIError::NetworkError(format!(
-                    "Failed while fetching assets status for {:?}: {:?}",
-                    key.deposit_address, e
-                ))
-            })?;
-
-            if assets_status.not_claimed_indices.is_empty() {
-                print_status(format!(
-                    "All eligible rewards are claimed for {:?}.",
-                    key.deposit_address
-                ));
-                break;
-            }
-
-            claim_task(state, key, &assets_status).await?;
-
-            common_loop_cool_down().await;
+pub async fn claim_loop(state: &mut State, withdrawal_private_key: H256) -> anyhow::Result<()> {
+    let mut key_number = 0;
+    loop {
+        let key = Key::new(withdrawal_private_key, key_number);
+        if !is_address_used(key.deposit_address).await {
+            return Ok(());
         }
+        print_status(format!("Claim loop for {:?}", key.deposit_address));
+        let assets_status = state.sync_and_fetch_assets(&key).await?;
+        claim_task(state, &key, &assets_status).await?;
+        common_loop_cool_down().await;
+        key_number += 1;
     }
+}
 
-    Ok(())
+async fn is_address_used(deposit_address: Address) -> bool {
+    get_account_nonce(deposit_address).await.unwrap() > 0
+        || get_balance(deposit_address).await.unwrap() > 0.into()
 }
 
 async fn common_loop_cool_down() {
