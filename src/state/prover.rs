760255568a1956ea1e019b5ccc6ac7f6b5408eaf
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-10-06 18:17:16 +0700

    circuit digest

--- src/state/prover.rs
+++ src/state/prover.rs
@@ -1,3 +1,4 @@
+use log::info;
 use mining_circuit_v1::{
     claim::{claim_processor::ClaimProcessor, claim_wrapper_processor::ClaimWrapperProcessor},
     withdrawal::simple_withdrawal_wrapper_processor::SimpleWithdrawalWrapperProcessor,
@@ -19,6 +20,25 @@ impl Prover {
         let withdrawal_wrapper_processor = SimpleWithdrawalWrapperProcessor::new();
         let claim_processor = ClaimProcessor::new();
         let claim_wrapper_processor = ClaimWrapperProcessor::new(&claim_processor.claim_circuit);
+
+        let withdrawal_digest = serde_json::to_string(
+            &withdrawal_wrapper_processor
+                .wrapper_circuit1
+                .data
+                .verifier_only
+                .circuit_digest,
+        )
+        .unwrap();
+        let claim_digest = serde_json::to_string(
+            &claim_wrapper_processor
+                .wrapper_circuit1
+                .data
+                .verifier_only
+                .circuit_digest,
+        )
+        .unwrap();
+        info!("Withdrawal digest: {}", withdrawal_digest);
+        info!("Claim digest: {}", claim_digest);
         Self {
             withdrawal_wrapper_processor,
             claim_processor,
