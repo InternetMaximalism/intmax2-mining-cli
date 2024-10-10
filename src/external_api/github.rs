f1f381b16d4c86e52b6229d20dabe513b6030420
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-10-10 14:16:00 +0700

    short long

--- src/external_api/github.rs
+++ src/external_api/github.rs
@@ -1,7 +1,7 @@
 use std::fs;
 
 use anyhow::Context;
-use chrono::{NaiveDate, NaiveDateTime};
+use chrono::NaiveDate;
 use log::info;
 use regex::Regex;
 use reqwest::{self, Client};
@@ -17,14 +17,28 @@ use crate::{
     },
 };
 
-pub async fn fetch_latest_tree_from_github(
-    last_update: NaiveDateTime,
-) -> anyhow::Result<Option<(BinDepositTree, BinEligibleTree, NaiveDateTime)>> {
+#[derive(Debug, thiserror::Error)]
+enum Error {
+    #[error("Failed to fetch data from GitHub")]
+    NetworkError,
+    #[error("Failed to deserialize data {}", _0)]
+    DeserializeError(String),
+}
+
+#[derive(Debug)]
+pub struct BinTrees {
+    pub bin_deposit_tree: Option<BinDepositTree>,
+    pub bin_short_term_eligible_tree: Option<BinEligibleTree>,
+    pub bin_long_term_eligible_tree: Option<BinEligibleTree>,
+    pub latest_update: NaiveDate,
+}
+
+pub async fn fetch_latest_tree_from_github(last_update: NaiveDate) -> Result<BinTrees, Error> {
     info!(
         "Fetching latest deposit and eligible trees from GitHub, last update: {}",
         last_update
     );
-    let settings = Settings::load()?;
+    let settings = Settings::load().unwrap();
     let client = reqwest::Client::new();
     let url = format!(
         "https://api.github.com/repos/{}/contents/{}?ref={}",
@@ -32,73 +46,129 @@ pub async fn fetch_latest_tree_from_github(
         settings.api.tree_data_directory,
         settings.api.tree_data_branch
     );
-    let response = client
+    let file_list = client
         .get(&url)
         .header("User-Agent", "Rust-GitHub-File-Reader")
         .send()
-        .await?
+        .await
+        .map_err(|_| Error::NetworkError)?
         .json::<Vec<Value>>()
         .await
-        .context("Failed to fetch data from GitHub")?;
+        .map_err(|e| {
+            Error::DeserializeError("failed to parse fetched files as array".to_string())
+        })?;
 
     let deposit_pattern = Regex::new(r"^\d{4}-\d{2}-\d{2}-depositTree\.txt$").unwrap();
-    let eligible_pattern = Regex::new(r"^\d{4}-\d{2}-\d{2}-eligibleTree\.txt$").unwrap();
-    let mut latest_deposit_file: Option<&Value> = None;
-    let mut latest_eligible_file: Option<&Value> = None;
-    let mut latest_deposit_date = last_update.date();
-    let mut latest_eligible_date = last_update.date();
+    let short_term_eligible_pattern =
+        Regex::new(r"^\d{4}-\d{2}-\d{2}-eligibleTree-shortTerm\.txt$").unwrap();
+    let long_term_eligible_pattern =
+        Regex::new(r"^\d{4}-\d{2}-\d{2}-eligibleTree-longTerm\.txt$").unwrap();
 
-    for file in response.iter() {
+    let (latest_deposit_date, latest_deposit_file) =
+        filter_file(&deposit_pattern, &file_list, last_update);
+    let (latest_short_term_eligible_date, latest_short_term_eligible_file) =
+        filter_file(&short_term_eligible_pattern, &file_list, last_update);
+    let (latest_long_term_eligible_date, latest_long_term_eligible_file) =
+        filter_file(&long_term_eligible_pattern, &file_list, last_update);
+
+    let bin_deposit_tree = if let Some(latest_deposit_file) = latest_deposit_file {
+        let content = fetch_content(&client, &latest_deposit_file).await?;
+        let bin_deposit_tree: BinDepositTree = bincode::deserialize(&content).map_err(|e| {
+            Error::DeserializeError(format!("failed to deserialize deposit tree: {}", e))
+        })?;
+        Some(bin_deposit_tree)
+    } else {
+        None
+    };
+
+    let bin_short_term_eligible_tree = if let Some(latest_short_term_eligible_file) =
+        latest_short_term_eligible_file
+    {
+        let content = fetch_content(&client, &latest_short_term_eligible_file).await?;
+        let bin_eligible_tree: BinEligibleTree = bincode::deserialize(&content).map_err(|e| {
+            Error::DeserializeError(format!(
+                "failed to deserialize short term eligible tree: {}",
+                e
+            ))
+        })?;
+        Some(bin_eligible_tree)
+    } else {
+        None
+    };
+
+    let bin_long_term_eligible_tree = if let Some(latest_long_term_eligible_file) =
+        latest_long_term_eligible_file
+    {
+        let content = fetch_content(&client, &latest_long_term_eligible_file).await?;
+        let bin_eligible_tree: BinEligibleTree = bincode::deserialize(&content).map_err(|e| {
+            Error::DeserializeError(format!(
+                "failed to deserialize short term eligible tree: {}",
+                e
+            ))
+        })?;
+        Some(bin_eligible_tree)
+    } else {
+        None
+    };
+    let latest_update = vec![
+        latest_deposit_date,
+        latest_short_term_eligible_date,
+        latest_long_term_eligible_date,
+    ]
+    .into_iter()
+    .max()
+    .unwrap(); // iter is never empty
+
+    Ok(BinTrees {
+        bin_deposit_tree,
+        bin_short_term_eligible_tree,
+        bin_long_term_eligible_tree,
+        latest_update,
+    })
+}
+
+fn filter_file(
+    pattern: &Regex,
+    file_list: &[Value],
+    prev_date: NaiveDate,
+) -> (NaiveDate, Option<Value>) {
+    let mut latest_file: Option<Value> = None;
+    let mut latest_date = prev_date;
+    for file in file_list {
         if let Some(name) = file["name"].as_str() {
             if name.len() < 10 {
+                // ignore files without date
                 continue;
             }
             if let Ok(date) = NaiveDate::parse_from_str(&name[0..10], "%Y-%m-%d") {
-                if deposit_pattern.is_match(name) && date > latest_deposit_date {
-                    latest_deposit_date = date;
-                    latest_deposit_file = Some(file);
-                } else if eligible_pattern.is_match(name) && date > latest_eligible_date {
-                    latest_eligible_date = date;
-                    latest_eligible_file = Some(file);
+                if pattern.is_match(name) && date > latest_date {
+                    latest_date = date;
+                    latest_file = Some(file.clone());
                 }
             }
         }
     }
-
-    if latest_deposit_file.is_none()
-        || latest_eligible_file.is_none()
-        || (latest_deposit_date <= last_update.date() && latest_eligible_date <= last_update.date())
-    {
-        return Ok(None);
+    if latest_file.is_none() || latest_date <= prev_date {
+        return (prev_date, None); // no new file
     }
-
-    let deposit_file = latest_deposit_file.unwrap();
-    let content = fetch_content(&client, deposit_file).await?;
-    let bin_deposit_tree: BinDepositTree = bincode::deserialize(&content)?;
-
-    let eligible_file = latest_eligible_file.unwrap();
-    let content = fetch_content(&client, eligible_file).await?;
-    let bin_eligible_tree: BinEligibleTree = bincode::deserialize(&content)?;
-
-    let latest_update = latest_deposit_date
-        .max(latest_eligible_date)
-        .and_hms_opt(0, 0, 0)
-        .unwrap();
-
-    Ok(Some((bin_deposit_tree, bin_eligible_tree, latest_update)))
+    return (latest_date, latest_file);
 }
 
-async fn fetch_content(client: &reqwest::Client, file: &Value) -> anyhow::Result<Vec<u8>> {
+async fn fetch_content(client: &reqwest::Client, file: &Value) -> Result<Vec<u8>, Error> {
     let download_url = file["download_url"]
         .as_str()
-        .ok_or(anyhow::anyhow!("No download URL"))?;
+        .ok_or(Error::DeserializeError(
+            "no download_url filed in github files".to_string(),
+        ))?;
     let content = client
         .get(download_url)
         .header("User-Agent", "Rust-GitHub-File-Reader")
         .send()
-        .await?
+        .await
+        .map_err(|_| Error::NetworkError)?
         .bytes()
-        .await?;
+        .await
+        .map_err(|_| Error::DeserializeError("failed to deserialize files as bytes".to_string()))?;
     Ok(content.into())
 }
 
