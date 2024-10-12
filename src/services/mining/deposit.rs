e4ccd22bc51a1e4dfc1bc99018b99e6bef540aff
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-10-12 14:37:46 +0700

    minor change

--- src/services/mining/deposit.rs
+++ src/services/mining/deposit.rs
@@ -29,8 +29,7 @@ pub async fn deposit_task(_state: &State, key: &Key, mining_unit: U256) -> anyho
         .deposit_native_token(pubkey_salt_hash)
         .value(mining_unit);
     tx.tx.set_nonce(nonce);
-
-    let tx_hash = handle_contract_call(tx, deposit_address, "deposit", "deposit").await?;
+    let _tx_hash = handle_contract_call(tx, deposit_address, "deposit", "deposit").await?;
     print_log(format!("Successfully deposited"));
     Ok(())
 }
