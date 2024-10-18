d29afa3155d0f8a812d443744a32a42b9eee442d
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-10-18 16:08:13 +0700

    change branch to main

--- src/constants.rs
+++ src/constants.rs
@@ -2,5 +2,4 @@ pub const REPO_OWNER: &str = "InternetMaximalism";
 pub const REPO_NAME: &str = "intmax2-mining-cli";
 pub const CONFIG_PATH: &str = "config";
 pub const TERM_OF_USE_PATH: &str = "docs/terms.md";
-// todo: change this to the main branch
-pub const BRANCH: &str = "base";
+pub const BRANCH: &str = "main";
