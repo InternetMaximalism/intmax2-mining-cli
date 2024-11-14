43c5fe65eb0a2eac50fc9e130492f2d5726ba1b3
kbizikav <132550763+kbizikav@users.noreply.github.com>
2024-11-14 11:44:54 +0700

    Dev1.1.6 (#34)
    * fix get_deposited_event_by_sender bug
    
    * add log
    
    * fix latest timestamp
    
    * skip duplicated tx
    
    * duplicate

--- src/external_api/contracts/events.rs
+++ src/external_api/contracts/events.rs
@@ -5,13 +5,15 @@ use intmax2_zkp::{
 };
 use log::info;
 
-use crate::{external_api::contracts::utils::get_block, utils::retry::with_retry};
+use crate::{external_api::contracts::utils::get_latest_block_number, utils::retry::with_retry};
 
 use super::{
     error::BlockchainError,
     int1::get_int1_contract,
     utils::{get_client, u256_as_bytes_be},
 };
+
+const EVENT_BLOCK_RANGE: u64 = 100000;
 
 #[derive(Clone, Debug)]
 pub struct Deposited {
@@ -38,20 +40,43 @@ pub async fn get_deposited_event_by_sender(
 ) -> Result<Vec<Deposited>, BlockchainError> {
     info!("get_deposited_event_by_sender");
     let int1 = get_int1_contract().await?;
-    let events = with_retry(|| async {
-        int1.deposited_filter()
-            .from_block(0)
-            .address(int1.address().into())
-            .topic2(sender)
-            .query_with_meta()
-            .await
-    })
-    .await
-    .map_err(|_| BlockchainError::NetworkError("failed to get deposited event".to_string()))?;
+
+    let settings = crate::utils::config::Settings::load().unwrap();
+    let mut events = Vec::new();
+    let mut from_block = settings.blockchain.int1_deployed_block;
+    loop {
+        info!("get_deposited_event_by_sender: from_block={}", from_block);
+        let new_events = with_retry(|| async {
+            int1.deposited_filter()
+                .address(int1.address().into())
+                .topic2(sender)
+                .from_block(from_block)
+                .to_block(from_block + EVENT_BLOCK_RANGE)
+                .query_with_meta()
+                .await
+        })
+        .await
+        .map_err(|_| BlockchainError::NetworkError("failed to get deposited event".to_string()))?;
+        events.extend(new_events);
+        let latest_block_number = get_latest_block_number().await?;
+        from_block += EVENT_BLOCK_RANGE;
+        if from_block > latest_block_number {
+            break;
+        }
+    }
+
     let client = get_client().await?;
     let mut deposited_events = Vec::new();
+    let mut seen_deposit_id = std::collections::HashSet::new();
     for (event, meta) in events {
-        // get tx_nonce  because it is needed for getting deposit salt
+        // skip duplicated deposit
+        let deposit_id = event.deposit_id;
+        if seen_deposit_id.contains(&deposit_id) {
+            continue;
+        } else {
+            seen_deposit_id.insert(deposit_id);
+        }
+
         let tx_hash = meta.transaction_hash;
         let tx = with_retry(|| async { client.get_transaction(tx_hash).await })
             .await
@@ -81,30 +106,18 @@ pub struct DepositLeafInserted {
 pub async fn get_deposit_leaf_inserted_event(
     from_block: u64,
 ) -> Result<Vec<DepositLeafInserted>, BlockchainError> {
-    const PAGE_SIZE: u64 = 500000;
+    log::info!("get_deposit_leaf_inserted_event");
 
-    let latest_block_number = get_client()
-        .await?
-        .get_block_number()
-        .await
-        .map_err(|_| {
-            BlockchainError::NetworkError("failed to get latest block number".to_string())
-        })?
-        .as_u64();
-    let mut flatten_events = Vec::new();
+    let mut events = Vec::new();
     let mut from_block = from_block;
-    let mut to_block = from_block + PAGE_SIZE - 1;
-    while from_block <= latest_block_number {
-        if to_block > latest_block_number {
-            to_block = latest_block_number;
-        }
-
+    loop {
+        log::info!("get_deposit_leaf_inserted_event: from_block={}", from_block);
         let int1 = get_int1_contract().await?;
-        let events = with_retry(|| async {
+        let new_events = with_retry(|| async {
             int1.deposit_leaf_inserted_filter()
                 .address(int1.address().into())
                 .from_block(from_block)
-                .to_block(to_block)
+                .to_block(from_block + EVENT_BLOCK_RANGE)
                 .query_with_meta()
                 .await
         })
@@ -112,75 +125,112 @@ pub async fn get_deposit_leaf_inserted_event(
         .map_err(|_| {
             BlockchainError::NetworkError("failed to get deposit leaf inserted event".to_string())
         })?;
-
-        let events: Vec<DepositLeafInserted> = events
-            .into_iter()
-            .map(|(event, meta)| DepositLeafInserted {
-                deposit_index: event.deposit_index,
-                deposit_hash: Bytes32::from_bytes_be(&event.deposit_hash),
-                block_number: meta.block_number.as_u64(),
-            })
-            .collect();
-        flatten_events.extend_from_slice(&events);
-
-        from_block = to_block + 1;
-        to_block = from_block + PAGE_SIZE - 1;
+        events.extend(new_events);
+        let latest_block_number = get_latest_block_number().await?;
+        from_block += EVENT_BLOCK_RANGE;
+        if from_block > latest_block_number {
+            break;
+        }
+    }
+    let mut deposit_leaf_inserted_events = Vec::new();
+    let mut seen_deposit_indices = std::collections::HashSet::new();
+    for (event, meta) in events {
+        // skip duplicated deposit
+        let deposit_index = event.deposit_index;
+        if seen_deposit_indices.contains(&deposit_index) {
+            continue;
+        } else {
+            seen_deposit_indices.insert(deposit_index);
+        }
+        deposit_leaf_inserted_events.push(DepositLeafInserted {
+            deposit_index: event.deposit_index,
+            deposit_hash: Bytes32::from_bytes_be(&event.deposit_hash),
+            block_number: meta.block_number.as_u64(),
+        });
     }
 
-    flatten_events.sort_by_key(|event| event.deposit_index);
-    Ok(flatten_events)
+    deposit_leaf_inserted_events.sort_by_key(|event| event.deposit_index);
+
+    Ok(deposit_leaf_inserted_events)
 }
 
 pub async fn get_latest_deposit_timestamp(sender: Address) -> Result<Option<u64>, BlockchainError> {
+    log::info!("get_latest_deposit_timestamp");
     let int1 = get_int1_contract().await?;
-    let events = with_retry(|| async {
-        int1.deposited_filter()
-            .from_block(0)
-            .address(int1.address().into())
-            .topic2(sender)
-            .query_with_meta()
-            .await
-    })
-    .await
-    .map_err(|_| BlockchainError::NetworkError("failed to get deposited event".to_string()))?;
-    let latest_block_number: Option<u64> = events
-        .into_iter()
-        .map(|(_, meta)| meta.block_number.as_u64())
-        .max();
-    let block_timestamp = if let Some(block_number) = latest_block_number {
-        let block = get_block(block_number).await?;
-        Some(block.unwrap().timestamp.as_u64())
-    } else {
-        None
-    };
-    Ok(block_timestamp)
+
+    let mut to_block = get_latest_block_number().await?;
+    let mut latest_event_block_number = None;
+    let int1_deployed_block = crate::utils::config::Settings::load()
+        .unwrap()
+        .blockchain
+        .int1_deployed_block;
+    loop {
+        let events = with_retry(|| async {
+            int1.deposited_filter()
+                .from_block(to_block.saturating_sub(EVENT_BLOCK_RANGE))
+                .to_block(to_block)
+                .address(int1.address().into())
+                .topic2(sender)
+                .query_with_meta()
+                .await
+        })
+        .await
+        .map_err(|_| BlockchainError::NetworkError("failed to get deposited event".to_string()))?;
+        let max_block_number: Option<u64> = events
+            .into_iter()
+            .map(|(_, meta)| meta.block_number.as_u64())
+            .max();
+        if max_block_number.is_some() {
+            latest_event_block_number = max_block_number;
+            break;
+        }
+        to_block = to_block.saturating_sub(EVENT_BLOCK_RANGE);
+        if to_block < int1_deployed_block {
+            break;
+        }
+    }
+
+    Ok(latest_event_block_number)
 }
 
 pub async fn get_latest_withdrawal_timestamp(
     recipient: Address,
 ) -> Result<Option<u64>, BlockchainError> {
+    log::info!("get_latest_withdrawal_timestamp");
     let int1 = get_int1_contract().await?;
-    let events = with_retry(|| async {
-        int1.withdrawn_filter()
-            .from_block(0)
-            .address(int1.address().into())
-            .topic1(recipient)
-            .query_with_meta()
-            .await
-    })
-    .await
-    .map_err(|_| BlockchainError::NetworkError("failed to get withdrawn event".to_string()))?;
-    let latest_block_number: Option<u64> = events
-        .into_iter()
-        .map(|(_, meta)| meta.block_number.as_u64())
-        .max();
-    let block_timestamp = if let Some(block_number) = latest_block_number {
-        let block = get_block(block_number).await?;
-        Some(block.unwrap().timestamp.as_u64())
-    } else {
-        None
-    };
-    Ok(block_timestamp)
+
+    let mut to_block = get_latest_block_number().await?;
+    let mut latest_event_block_number = None;
+    let int1_deployed_block = crate::utils::config::Settings::load()
+        .unwrap()
+        .blockchain
+        .int1_deployed_block;
+    loop {
+        let events = with_retry(|| async {
+            int1.withdrawn_filter()
+                .from_block(to_block.saturating_sub(EVENT_BLOCK_RANGE))
+                .to_block(to_block)
+                .address(int1.address().into())
+                .topic2(recipient)
+                .query_with_meta()
+                .await
+        })
+        .await
+        .map_err(|_| BlockchainError::NetworkError("failed to get withdrawn event".to_string()))?;
+        let max_block_number: Option<u64> = events
+            .into_iter()
+            .map(|(_, meta)| meta.block_number.as_u64())
+            .max();
+        if max_block_number.is_some() {
+            latest_event_block_number = max_block_number;
+            break;
+        }
+        to_block = to_block.saturating_sub(EVENT_BLOCK_RANGE);
+        if to_block < int1_deployed_block {
+            break;
+        }
+    }
+    Ok(latest_event_block_number)
 }
 
 #[cfg(test)]
