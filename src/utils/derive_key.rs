f940c3ba8fb58939a4c5858225c32ec40874a622
kbizikav <132550763+kbizikav@users.noreply.github.com>
2025-05-28 21:11:46 +0700

    chore: add derive test

--- src/utils/derive_key.rs
+++ src/utils/derive_key.rs
@@ -13,12 +13,10 @@ pub fn derive_salt_from_private_key_nonce(private_key: B256, nonce: u64) -> Salt
         .concat()
         .to_vec();
     let hashed_private_key = keccak256(keccak256(&prefixed_private_key));
-    let hashed_private_key_with_nonce =
-        [hashed_private_key.to_vec(), nonce.to_be_bytes().to_vec()]
-            .concat()
-            .to_vec();
-    let salt_bytes =
-        Bytes32::from_bytes_be(keccak256(&hashed_private_key_with_nonce).as_ref());
+    let hashed_private_key_with_nonce = [hashed_private_key.to_vec(), nonce.to_be_bytes().to_vec()]
+        .concat()
+        .to_vec();
+    let salt_bytes = Bytes32::from_bytes_be(keccak256(&hashed_private_key_with_nonce).as_ref());
     Salt(salt_bytes.reduce_to_hash_out())
 }
 
@@ -48,6 +46,47 @@ pub fn derive_deposit_private_key(withdrawal_private_key: B256, number: u64) -> 
         [hashed_private_key.to_vec(), number.to_be_bytes().to_vec()]
             .concat()
             .to_vec();
-    
+
     keccak256(&hashed_private_key_with_number)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use alloy::primitives::B256;
+
+    fn private_key() -> B256 {
+        "0x8d7a905dad7deda35996c7127b0d6dd9921f7d45e3f8dce86e09370265bb7571"
+            .parse()
+            .unwrap()
+    }
+
+    #[test]
+    fn test_derive_salt_from_private_key_nonce() {
+        let nonce = 1;
+        let salt = derive_salt_from_private_key_nonce(private_key(), nonce);
+        assert_eq!(
+            salt.to_string(),
+            "0xf55e011dcea2bda3f2221d4ac872f681ce6b97dc9490c8d89afafb4e211d4ecc"
+        );
+    }
+
+    #[test]
+    fn test_derive_pubkey_from_private_key() {
+        let pubkey = derive_pubkey_from_private_key(private_key());
+        assert_eq!(
+            pubkey.to_string(),
+            "102824996597214675512080228570082669559954049667357114099795180313928773990100"
+        );
+    }
+
+    #[test]
+    fn test_derive_deposit_private_key() {
+        let number = 1;
+        let deposit_private_key = derive_deposit_private_key(private_key(), number);
+        assert_eq!(
+            deposit_private_key.to_string(),
+            "0x724bfaba8fdaa147295f428cc52c64d012c93056795d4c78ccdcb0d074e636c3"
+        );
+    }
 }
